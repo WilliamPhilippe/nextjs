bdd4255c9745c4f11c5f18c2283e31083e2be1d8
William Philippe <ctt.williamphilippe@gmail.com>
2022-09-04 23:29:27 -0300

    feat: getStatic paths and props

---
+++ nextjs-blog/pages/posts/[id].js
@@ -0,0 +1,30 @@
+import Layout from "../../components/layout";
+
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+export async function getStaticPaths() {
+  return {
+    paths: getAllPostIds(),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
+}
